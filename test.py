f61857d227245e2d75b1bd76b56120a0453ebb84
Max-Pc <82034134+Max-Pc-Prgm@users.noreply.github.com>
2023-05-26 12:52:34 +0530

    Update test.py

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ sum = num1 + num2
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
 
 # Access the system parameter as an environment variable
-my_param = %system.key1%
+my_param = %env.1%
 
 # Use the system parameter in your script
 print("My Param:", my_param)
