612804fb76ab9fec9161ff16b9ccfae567561885
Max-Pc <82034134+Max-Pc-Prgm@users.noreply.github.com>
2023-05-29 18:23:59 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,13 +4,13 @@ num1 = 1.5
 num2 = 6.3
 
 # Add two numbe
-sum = num1 + 
+sum = num1 + num2
 
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
 
 # Access the system parameter as an environment variable
 
-parameter_value = os.environ.get("env.1")
+parameter_value = os.environ.get("1")
 # Use the system parameter in your script
 print(parameter_value)
